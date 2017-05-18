26cc3b2b41fd269d5e3129563b32b18a23f40c82
Mathieu Virbel <mat@kivy.org>
2017-05-18 21:20:02 +0200

    another approach: use kinetic

--- smoothtouch.py
+++ smoothtouch.py
@@ -6,6 +6,22 @@ from kivy.clock import Clock
 from kivy.core.window import Window as win
 from kivy.utils import platform
 from functools import partial
+from time import time
+from kivy.effects.kinetic import KineticEffect
+
+
+class CustomKineticEffect(KineticEffect):
+    def calculate_velocity(self):
+        newest_sample = self.history[-1]
+        old_sample = self.history[0]
+        for sample in self.history:
+            if (newest_sample[0] - sample[0]) < 10. / 60.:
+                break
+            old_sample = sample
+        distance = newest_sample[1] - old_sample[1]
+        duration = abs(newest_sample[0] - old_sample[0])
+        self.velocity = (distance / max(duration, 0.0001))
+        return self.velocity
 
 
 class SmoothMotionEvent(MotionEvent):
@@ -37,6 +53,12 @@ class SmoothTouch(BoxLayout):
             me.button = touch.button
         me.target_sx = me.sx
         me.target_sy = me.sy
+        me.got_a_move = False
+        me.last_move = time()
+        me.kx = CustomKineticEffect()
+        me.ky = CustomKineticEffect()
+        me.kx.start(me.sx)
+        me.ky.start(me.sy)
         self.touches[touch] = me
         self.simulate_touch_down(me)
         return True
@@ -49,6 +71,10 @@ class SmoothTouch(BoxLayout):
             sy = y / win.height
             me.target_sx = sx
             me.target_sy = sy
+            me.kx.update(sx)
+            me.ky.update(sy)
+            me.got_a_move = True
+            me.last_move = time()
             return True
 
     def on_touch_up(self, touch):
@@ -70,14 +96,15 @@ class SmoothTouch(BoxLayout):
         me.pop()
 
     def simulate_touch_move(self, me, dt):
-        tx = (me.target_sx - me.sx)
-        ty = (me.target_sy - me.sy)
-        sx = me.sx + tx / 4.
-        sy = me.sy + ty / 4.
+        # tx = (me.target_sx - me.sx)
+        # ty = (me.target_sy - me.sy)
+        # sx = me.sx + tx / 4.
+        # sy = me.sy + ty / 4.
         w, h = win.system_size
         if platform == "ios" or win._density != 1:
             w, h = win.size
-        me.move((sx, sy))
+
+        self.update_touch(me, dt)
         me.scale_for_screen(w, h)
         me.push()
         me.apply_transform_2d(self.to_widget)
@@ -96,6 +123,23 @@ class SmoothTouch(BoxLayout):
         super(SmoothTouch, self).on_touch_up(me)
         me.pop()
         self.dispatch_grab("up", me)
+
+    def update_touch(self, me, dt):
+        if me.got_a_move:
+            me.got_a_move = False
+            me.kx.update(me.target_sx)
+            me.ky.update(me.target_sy)
+            # me.move((me.target_sx, me.target_sy))
+            me.kx.calculate_velocity()
+            me.ky.calculate_velocity()
+        else:
+            # after 0.1s, clear velocity asap
+            if time() - me.last_move > 0.1:
+                me.kx.velocity /= 2.
+                me.ky.velocity /= 2.
+        sx = me.sx + me.kx.velocity * dt
+        sy = me.sy + me.ky.velocity * dt
+        me.move((sx, sy))
 
     def dispatch_grab(self, etype, me):
         me.grab_state = True
